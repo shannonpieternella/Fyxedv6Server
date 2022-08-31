325d5bd1a6bf634fc7d47c602426ddc86f8f922b
Shannon Pieternella <shannonpieternella@Shannons-MacBook-Pro.local>
2022-08-31 22:30:27 +0200

    make it better

--- routes/posts.js
+++ routes/posts.js
@@ -919,33 +919,38 @@ console.log('testwerkt')
               if( req.body.bedrijfsnaam == 'naambedrijf' ){
                 console.log('naam')
                 const updatedPost = await Companies.findByIdAndUpdate({_id: req.body.id}, { $set: { Bedrijfsnaam: req.body.officialname}});
-                res.json(req.body.Beschrijving);
+              
               }
 
               if( req.body.stadnu == 'stad' ){
                 console.log('naam')
-                const updatedPost = await Companies.findByIdAndUpdate({_id: '621092d8c01b17ebb4cba0a8'}, { $set: { Stad: req.body.stadnu}});
-                res.json(req.body.Beschrijving);
+                const updatedPost = await Companies.findByIdAndUpdate({_id: req.body.id}, { $set: { Stad: req.body.officialstad}});
+              
               }
 
               if( req.body.maplinknu == 'maplink' ){
                 console.log('naam')
-                const updatedPost = await Companies.findByIdAndUpdate({_id: '621092d8c01b17ebb4cba0a8'}, { $set: { maplink: req.body.maplinknu}});
-                res.json(req.body.Beschrijving);
+                const updatedPost = await Companies.findByIdAndUpdate({_id: req.body.id}, { $set: { maplink: req.body.maplinkofficial}});
+              
               }
 
               if( req.body.profielimagenu == 'profielimage' ){
                 console.log('naam')
-                const updatedPost = await Companies.findByIdAndUpdate({_id: '621092d8c01b17ebb4cba0a8'}, { $set: { Imagenew: req.body.profielimagenu}});
-                res.json(req.body.Beschrijving);
+                const updatedPost = await Companies.findByIdAndUpdate({_id: req.body.id}, { $set: { Imagenew: req.body.profielimageofficial}});
+             
               }
 
               if( req.body.typebedrijfnu == 'bedrijfstype' ){
                 console.log('naam')
-                const updatedPost = await Companies.findByIdAndUpdate({_id: '621092d8c01b17ebb4cba0a8'}, { $set: { Bedrijfstype: req.body.bedrijfstypenu}});
-                res.json(req.body.Beschrijving);
+                const updatedPost = await Companies.findByIdAndUpdate({_id: req.body.id}, { $set: { Bedrijfstype: req.body.bedrijfstypeofficial}});
+                
               }
 
+              if( req.body.typebeschrijving == 'beschrijving' ){
+                console.log('naam')
+                const updatedPost = await Companies.findByIdAndUpdate({_id: req.body.id}, { $set: { Bedrijfstype: req.body.bedrijfsomschrijvingofficial}});
+                res.json(req.body.bedrijfstypeofficial);
+              }
 
 
 
